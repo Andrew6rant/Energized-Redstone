0f89a9359b24b9a10f443a25d08552a0ae94702a
Andrew Grant <andrew.grant@mail.rit.edu>
2022-08-13 16:00:39 -0400

    Initial commit

---
+++ src/main/java/io/github/Andrew6rant/energized_redstone/EnergizedRedstone.java
@@ -0,0 +1,10 @@
+package io.github.Andrew6rant.energized_redstone;
+
+import net.fabricmc.api.ModInitializer;
+
+public class EnergizedRedstone implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
